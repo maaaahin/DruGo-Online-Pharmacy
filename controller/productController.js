35ea5349dc601a1714711ae17e51f184bda2e37f
MAHIN CHANDWANI <mahinchandwani2@gmail.com>
2025-06-22 16:27:58 +0530

    Build2

--- controller/productController.js
+++ controller/productController.js
@@ -6,13 +6,7 @@ import slugify from "slugify";
 import braintree from "braintree";
 import dotenv from "dotenv";
 dotenv.config();
-//payment gateway
-var gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
-});
+
 
 export const createProductController = async (req, res) => {
   try {
@@ -325,63 +319,6 @@ export const productCategoryController = async (req, res) => {
       error,
       message: "Error While Getting products",
     });
-  }
-};
-
-// braintree token
-export const braintreeTokenController = async (req, res) => {
-  try {
-    gateway.clientToken.generate({}, function (err, response) {
-      if (err) {
-        res.status(500).send({
-          success: false,
-          message: "Error while generating token",
-          error: err,
-        });
-      }
-      res.send(response);
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      success: false,
-      message: "Error while generating token",
-      error,
-    });
-  }
-}
-
-//payment
-export const brainTreePaymentController = async (req, res) => {
-  try {
-    const { nonce, cart } = req.body;
-    let total = 0;
-    cart.map((i) => {
-      total += i.price;
-    });
-    let newTransaction = gateway.transaction.sale(
-      {
-        amount: total,
-        paymentMethodNonce: nonce,
-        options: {
-          submitForSettlement: true,
-        },
-      },
-      function (error, result) {
-        if (result) {
-          const order = new orderModel({
-            products: cart,
-            payment: result,
-            buyer: req.user._id,
-          }).save();
-          res.json({ ok: true });
-        } else {
-          res.status(500).send(error);
-        }
-      }
-    );
-  } catch (error) {
-    console.log(error);
   }
 };
 
